db6a9e3a697e4e35655fe0e20b153b0911b67292
JimmyBowcott <151764691+JimmyBowcott@users.noreply.github.com>
2024-07-10 10:30:40 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Battleship
-Play a game of battleship. See if you can beat the computer!
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { init } from './controller.js';
-
-// If you're looking for inspiration, the code is awful but it works.
-
-init();
